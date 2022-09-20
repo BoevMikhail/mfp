2a61eed9f466fc9a447fc67a93aaa5179d6c2471
BoevMikhail <92880566+BoevMikhail@users.noreply.github.com>
2022-09-20 13:02:35 +0300

    Some changes, delete trash

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,7 +4,7 @@ import MyInput from "./Ui/input/MyInput";
 
 const PostForm = ({create}) => {
     const [post, setPost] = useState({title: '', body: ''});
-    
+
     const addNewPost = (e) => {
         e.preventDefault()
         const newPost = {
@@ -12,14 +12,14 @@ const PostForm = ({create}) => {
         }
         create(newPost)
         setPost({title: '', body: ''})
-      }  
+      }
 
     return (
         <form className="AddForm">
             <fieldset className="BlockPost">
             <legend>Добавление поста</legend>
             <MyInput placeholder='Название поста' value={post.title} onChange={event => setPost({...post, title: event.target.value})}/>
-            <MyInput placeholder='Название поста' value={post.body} onChange={event => setPost({...post, body: event.target.value})}/>
+            <MyInput placeholder='Содержание поста' value={post.body} onChange={event => setPost({...post, body: event.target.value})}/>
             <MyButton onClick={addNewPost}>Нажми меня!!!</MyButton>
             </fieldset>
         </form>
