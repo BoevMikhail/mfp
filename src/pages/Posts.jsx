50c9a2465c6057e9dfab41a9ee962b836fe59320
BoevMikhail <92880566+BoevMikhail@users.noreply.github.com>
2022-10-03 13:45:30 +0300

    gh-pages

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -9,13 +9,12 @@ import PostService from "../API/PostService";
 import Loader from "../components/Ui/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../components/utils/pages";
-import Pagination from "../components/Ui/pagination/Pagination";
 import { useObserver } from "../hooks/useObserver";
 import MySelect from "../components/Ui/selecet/MySelect";
 
 function Posts() {
 	const [posts, setPosts] = useState([]);
-	const [filter, setFilter] = useState({ sort: '', query: '' });
+	const [filter, setFilter] = useState({ sort: "", query: "" });
 	const [modal, setModal] = useState(false);
 	const [totalPages, setTotalPages] = useState(0);
 	const [limit, setLimit] = useState(10);
@@ -23,32 +22,33 @@ function Posts() {
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 	const lastElement = useRef();
 
-
 	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
 		const response = await PostService.getAll(limit, page);
 		setPosts([...posts, ...response.data]);
-		const totalCount = response.headers['x-total-count'];
+		const totalCount = response.headers["x-total-count"];
 		setTotalPages(getPageCount(totalCount, limit));
-	})
+	});
 
-	useObserver(lastElement, page < totalPages, isPostsLoading, () => setPage(page + 1))
+	useObserver(lastElement, page < totalPages, isPostsLoading, () =>
+		setPage(page + 1)
+	);
 
 	useEffect(() => {
 		fetchPosts();
-	}, [page, limit])
+	}, [page, limit]);
 
 	const createPost = (newPost) => {
-		setPosts([...posts, newPost])
-		setModal(false)
-	}
+		setPosts([...posts, newPost]);
+		setModal(false);
+	};
 
 	const removePost = (post) => {
-		setPosts(posts.filter(p => p.id !== post.id))
-	}
+		setPosts(posts.filter((p) => p.id !== post.id));
+	};
 
 	const changePage = (page) => {
 		setPage(page);
-	}
+	};
 
 	return (
 		<div className="App">
@@ -56,41 +56,36 @@ function Posts() {
 			<PostFilter filter={filter} setFilter={setFilter} />
 			<MySelect
 				value={limit}
-				onChange={value => setLimit(value)}
+				onChange={(value) => setLimit(value)}
 				defaultValue="Количество элементов на странице"
 				options={[
-					{value: 5, name: '5'},
-					{value: 10, name: '10'},
-					{value: 25, name: '25'},
-					{value: -1, name: 'Показать всё'}
+					{ value: 5, name: "5" },
+					{ value: 10, name: "10" },
+					{ value: 25, name: "25" },
+					{ value: -1, name: "Показать всё" },
 				]}
 			/>
-			<Pagination
-				changePage={changePage}
-				page={page}
-				totalPages={totalPages}
-			/>
-			{postError &&
-				<h1>Произошла ошибка ${postError}</h1>
-			}
+			{postError && <h1>Произошла ошибка ${postError}</h1>}
 
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов' />
-			<div ref={lastElement} style={{ height: 50}}></div>
-			{isPostsLoading &&
-				<div style={{ display: 'flex', justifyContent: "center", marginTop: '50' }}>
+			<PostList
+				remove={removePost}
+				posts={sortedAndSearchedPosts}
+				title="Список постов"
+			/>
+			<div ref={lastElement} style={{ height: 50 }}></div>
+			{isPostsLoading && (
+				<div
+					style={{ display: "flex", justifyContent: "center", marginTop: "50" }}
+				>
 					<Loader />
 				</div>
-			}
-
+			)}
 
 			<MyModal visible={modal} setVisivle={setModal}>
 				<PostForm create={createPost} />
 			</MyModal>
-
 		</div>
-	)
+	);
 }
 
 export default Posts;
-
-
